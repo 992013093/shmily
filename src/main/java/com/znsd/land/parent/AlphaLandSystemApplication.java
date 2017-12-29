0f83779250e3a376ab23e04124d7774bea1e28a2
992013093 <992013093@qq.com>
2017-12-29 16:48:13 +0800

    [2017-12-29 10:57:33] 上传项目基础文件
    Signed-off-by: 992013093 <992013093@qq.com>

---
+++ src/main/java/com/znsd/land/parent/AlphaLandSystemApplication.java
@@ -0,0 +1,12 @@
+package com.znsd.land.parent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphaLandSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlphaLandSystemApplication.class, args);
+	}
+}
---
+++ src/test/java/com/znsd/land/parent/AlphaLandSystemApplicationTests.java
@@ -0,0 +1,16 @@
+package com.znsd.land.parent;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AlphaLandSystemApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
